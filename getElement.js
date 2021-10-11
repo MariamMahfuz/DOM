0a09a0cbe4e4e7b66b8cdd4ff32c608d868c5479
MariamMahfuz <bintemahfuz92@gmail.com>
2021-10-11 15:36:06 -0700

    first commit

---
+++ getElement.js
@@ -0,0 +1,8 @@
+var myHeading=document.getElementById("heading");
+myHeading.innerHTML="Hello,am Mariam";
+
+var myHeading2=document.getElementsByClassName("heading");
+myHeading2.innerHTML="Hi";
+
+var myHeading3=document.querySelector(".about");
+myHeading3.innerHtml="am a beginner in the sector of web development"
